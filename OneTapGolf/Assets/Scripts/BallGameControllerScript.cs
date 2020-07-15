bd4df744373c9606c64f8ebef82715d946425195
boguwyrw <b.wyrwal@wp.pl>
2020-07-15 21:20:19 +0200

    Game functionality has been completed
    Task:
    1. Visualization of the ball trajectory

--- OneTapGolf/Assets/Scripts/BallGameControllerScript.cs
+++ OneTapGolf/Assets/Scripts/BallGameControllerScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class BallGameControllerScript : MonoBehaviour
@@ -13,82 +14,95 @@ public class BallGameControllerScript : MonoBehaviour
     [SerializeField]
     private Transform parabolaPoint;
     [SerializeField]
+    private Text startGameText;
+    [SerializeField]
     private Text pointsText;
+    [SerializeField]
+    private GameObject gameOverObject;
+    [SerializeField]
+    private Text gameOverText;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text bestScoreText;
+    [SerializeField]
+    private Button restartButton;
 
-    private float maxDistance;
+    private float maxDistanceX;
     private float cameraDistance;
     private float holePositionX;
     private float flagPositionX;
-
     private int points = 0;
     private int currentParabolaPointPosition;
     private int nextParabolaPointPosition;
     private int force;
     private Rigidbody2D rb2D;
+    private Vector2 ballStartPosition;
+    private Vector2 parabolaPointStartPosition;
+    private float parabolaPointSpeed;
+    private bool isGameStart;
+
+    public static int bestScore = 0;
 
     private void Awake ()
     {
         cameraDistance = Vector3.Distance(transform.position , Camera.main.transform.position);
         Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1.0f, 1.0f, cameraDistance));
-        maxDistance = topCorner.x - 1.0f;
+        maxDistanceX = topCorner.x;
 
         ChangingHolePosition();
 
         parabolaPoint.position = transform.position;
 
+        startGameText.transform.position = new Vector3(0.5f * Screen.width, 0.65f * Screen.height, 0.0f);
         pointsText.transform.position = new Vector3(0.9f * Screen.width, 0.85f * Screen.height, 0.0f);
+
+        gameOverText.transform.position = new Vector3(0.5f * Screen.width, 0.7f * Screen.height, 0.0f);
+        scoreText.transform.position = new Vector3(0.3f * Screen.width, 0.5f * Screen.height, 0.0f);
+        bestScoreText.transform.position = new Vector3(0.7f * Screen.width, 0.5f * Screen.height, 0.0f);
+        restartButton.transform.position = new Vector3(0.5f * Screen.width, 0.3f * Screen.height, 0.0f);
+
         ShowingPoints();
 
-        currentParabolaPointPosition = (int)parabolaPoint.transform.localPosition.x;
-        nextParabolaPointPosition = currentParabolaPointPosition + 1;
+        RestartParabolaPointPosition();
+
         force = 0;
         rb2D = GetComponent<Rigidbody2D>();
+
+        ballStartPosition = new Vector2(transform.position.x, transform.position.y);
+        parabolaPointStartPosition = new Vector2(parabolaPoint.transform.position.x, parabolaPoint.transform.position.y);
+        parabolaPointSpeed = 1;
+        isGameStart = false;
+        gameOverObject.SetActive(false);
     }
 
     private void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            ChangingHolePosition();
-        }
-
         ShowingPoints();
         ParabolaImpactForce();
 
         currentParabolaPointPosition = (int)parabolaPoint.transform.localPosition.x;
-        Debug.Log("currentParabolaPointPosition: " + currentParabolaPointPosition);
-        Debug.Log("nextParabolaPointPosition: " + nextParabolaPointPosition);
+
         if (currentParabolaPointPosition == nextParabolaPointPosition)
         {
             force++;
             nextParabolaPointPosition = currentParabolaPointPosition + 1;
         }
-        Debug.Log(force);
-        /*
+        
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            transform.localPosition = new Vector2(parabolaPoint.position.x, parabolaPoint.position.y);
+            BallIsFly();
         }
-        */
-        if (Input.GetKeyUp(KeyCode.Space))
+
+        if (bestScore < points)
         {
-            rb2D.AddForce(transform.up * force, ForceMode2D.Impulse);
-            rb2D.AddForce(transform.right * force, ForceMode2D.Impulse);
+            bestScore = points;
         }
     }
-    /*
-    void FixedUpdate()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            rb2D.AddForce(transform.up * Mathf.Abs(currentParabolaPointPosition), ForceMode2D.Impulse);
-            rb2D.AddForce(transform.right * Mathf.Abs(currentParabolaPointPosition), ForceMode2D.Impulse);
-        }
-    }
-    */
+
     private void ChangingHolePosition()
     {
-        holePositionX = Random.Range(0, maxDistance);
+        holePositionX = Random.Range(0, maxDistanceX - 1.0f);
         flagPositionX = holePositionX;
         hole.position = new Vector2(holePositionX, hole.transform.position.y);
         flag.position = new Vector2(flagPositionX, flag.transform.position.y);
@@ -103,16 +117,64 @@ public class BallGameControllerScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            parabolaPoint.transform.Translate(Vector2.right * Time.deltaTime);
+            startGameText.gameObject.SetActive(false);
+            rb2D.constraints = RigidbodyConstraints2D.None;
+            parabolaPoint.transform.Translate(Vector2.right * parabolaPointSpeed * Time.deltaTime);
+            if (parabolaPoint.transform.localPosition.x >= maxDistanceX)
+            {
+                parabolaPoint.transform.Translate(Vector2.zero);
+                BallIsFly();
+            }
+            isGameStart = true;
         }
     }
 
+    private void RestartParabolaPointPosition()
+    {
+        currentParabolaPointPosition = (int)parabolaPoint.transform.localPosition.x;
+        nextParabolaPointPosition = currentParabolaPointPosition + 1;
+    }
+
+    private void BallIsFly()
+    {
+        Vector2 direction = new Vector2(1, 1);
+        rb2D.AddForce(direction * Mathf.Sqrt(Mathf.Abs(Physics.gravity.y) * force / 2), ForceMode2D.Impulse);
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Hole"))
+        
+        if(collision.gameObject.CompareTag("Hole"))
         {
-            points++;
+            isGameStart = false;
+            points += 1;
+            ChangingHolePosition();
+            transform.position = ballStartPosition;
+            rb2D.constraints = RigidbodyConstraints2D.FreezeAll;
+            parabolaPoint.transform.position = parabolaPointStartPosition;
+            force = 0;
+            RestartParabolaPointPosition();
+            parabolaPointSpeed++;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (isGameStart)
+            {
+                scoreText.text = "SCORE: " + points.ToString();
+                bestScoreText.text = "BEST: " + bestScore.ToString();
+                gameOverObject.SetActive(true);
+                rb2D.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+        }
+    }
+
+    public void RestartGameButton()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 
 }
